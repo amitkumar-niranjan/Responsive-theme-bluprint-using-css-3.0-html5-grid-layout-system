e2b107114c3b9e338142fcbf45bde760ce2bd032
amitkumar.niranjan <95871470+amitkumar-niranjan@users.noreply.github.com>
2022-07-29 14:39:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?PHP get_header(); ?>
 <div class="container">
-  <div id="logo"> logo</div>
-  <nav> 
+  <div id="logo">
+    <img src="https://images.unsplash.com/photo-1657420157348-0cf3a3375120?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTkwODM1MDM&ixlib=rb-1.2.1&q=80" />
+  </div>
+  <nav>
     <div class="menu-header-container">
       <ul id="menu-header-1" class="menu">
         <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-70"><a href="#">Home</a></li>
         <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-71"><a href="#">About</a></li>
         <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-72"><a href="#">Services</a></li>
         <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-73"><a href="#">Contact</a></li>
-</ul></div></nav>
-  <div id="banner">site banner</div>
+      </ul>
+    </div>
+  </nav>
+  <div id="banner">
+    <img src="https://images.unsplash.com/photo-1656828051768-5ef859415a89?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTkwODM1MDM&ixlib=rb-1.2.1&q=80" />
+
+  </div>
   <div id="login">
     <h1>Login Panel</h1>
     <form id="register">
